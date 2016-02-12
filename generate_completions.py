64aa231651bf8682f373bf76eb63945794475991
Grégoire Sage <gregoire.sage@gmail.com>
2016-02-12 23:33:42 +0100

    use argparse library

--- generate_completions.py
+++ generate_completions.py
@@ -7,7 +7,7 @@ Usage : python generate.py /path/to/pebble.h /path/to/pebble.sublime-completions
 """
 
 import sys
-import getopt
+import argparse
 import re
 
 def generate(header, output):
@@ -43,27 +43,10 @@ def generate(header, output):
 	f.write(content)
 	f.close()
 
-
-def main():
-	# parse command line options
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
-	except getopt.error, msg:
-		print msg
-		print "for help use --help"
-		sys.exit(2)
-	# process options
-	for o, a in opts:
-		if o in ("-h", "--help"):
-			print __doc__
-			sys.exit(0)
-	# process arguments
-	if(len(args) == 2):
-		generate(args[0], args[1])
-	else:
-		print "2 arguments are expected"
-		print "for help use --help"
-		sys.exit(2)
-
 if __name__ == "__main__":
-	sys.exit(main())
+	parser = argparse.ArgumentParser(description='Generate sublime-completions file from Pebble header file.')
+	parser.add_argument('header', action="store", help="Pebble header file")
+	parser.add_argument('output', action="store", help="sublime-completions output file")
+	result = parser.parse_args()
+
+	generate(result.header, result.output)
